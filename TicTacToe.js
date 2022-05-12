8a493af2fe9f6dc170893e767a8746004e7d8379
kitti2309 <kitti.ton2309@gmail.com>
2022-05-12 11:41:59 +0700

    edit code

--- TicTacToe.js
+++ TicTacToe.js
@@ -26,7 +26,7 @@ function VerifyTicTacTeo(TicTacToe){
 function checkcondition(check,val){
   var Win = [{x:0,y:1,z:2},{x:3,y:4,z:5},{x:6,y:7,z:8},
   			{x:0,y:3,z:6},{x:1,y:4,z:7},{x:2,y:5,z:8},
-              {x:0,y:4,z:8},{x:2,y:4,z:6}];
+            {x:0,y:4,z:8},{x:2,y:4,z:6}];
               var  res = false; 
        for(var i of Win){
        if(check[i.x] == val&&check[i.y]==val&&check[i.z]==val)
