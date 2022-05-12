3f8f4f46cd047dbb50fbbfa582e7570dfccb8e31
kitti2309 <kitti.ton2309@gmail.com>
2022-05-12 11:30:43 +0700

    edit

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,5 +1,5 @@
-var TicTacToe=[[1,2,1],
-               [2,2,1],
+var TicTacToe=[[1,1,2],
+               [2,1,2],
                [2,1,1]];
 function VerifyTicTacTeo(TicTacToe){
     let check =[];
@@ -9,8 +9,10 @@ function VerifyTicTacTeo(TicTacToe){
      }
     });
     var index = check.indexOf(0);
+    
     let x = checkcondition(check,1);
     let o = checkcondition(check,2);
+    
     if(x == true && o == false){return "X Win";}
     else if(o == true && x == false){return "O Win";}
     else if(o == true && x == true ){return "Draw";}
@@ -22,19 +24,18 @@ function VerifyTicTacTeo(TicTacToe){
   
 }
 function checkcondition(check,val){
-   if(check[0]==val&& check[1] == val&&check[2] == val 
-    ||check[3]==val&& check[4]== val&&check[5] == val
-    ||check[6]==val&& check[7] == val&&check[8] == val
-    ||check[0]==val&& check[3] == val&&check[6] == val
-    ||check[1]==val&& check[4] == val&&check[7] == val
-    ||check[2]==val&& check[5] == val&&check[8] == val
-    ||check[0]==val&& check[4] == val&&check[8] == val
-    ||check[2]==val&& check[4] == val&&check[6] == val){
-      return true;
-  }else{
-       return false;
-  }
-    
-
+  var Win = [{x:0,y:1,z:2},{x:3,y:4,z:5},{x:6,y:7,z:8},
+  						{x:0,y:3,z:5},{x:1,y:4,z:7},{x:2,y:5,z:8},
+              {x:0,y:4,z:8},{x:2,y:4,z:6}];
+              var  res = false; 
+       for(var i of Win){
+       if(check[i.x] == val&&check[i.y]==val&&check[i.z]==val)
+         {
+          res = true; 
+         }
+           
+       } 
+       return res
 }
- console.log(VerifyTicTacTeo(TicTacToe));    
+
+ console.log(VerifyTicTacTeo(TicTacToe));   
